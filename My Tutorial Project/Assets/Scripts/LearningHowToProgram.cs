b982565caaf3ec3bf6a4211adaad88500fb80a2a
Galaxomas <thefidanabdulla@gmail.com>
2022-11-21 18:45:45 +0400

    beginner tutorials

---
+++ My Tutorial Project/Assets/Scripts/LearningHowToProgram.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningHowToProgram : MonoBehaviour
+{
+
+    private Rigidbody2D myBody;
+    private BoxCollider2D myCollider;
+    private AudioSource audioSource;
+    private Animator anim;
+
+    private Transform myTransform;
+
+
+    //Player warrior;
+    private void Start()
+    {
+        anim= GetComponent<Animator>();
+
+        // for test
+
+    }
+
+}
