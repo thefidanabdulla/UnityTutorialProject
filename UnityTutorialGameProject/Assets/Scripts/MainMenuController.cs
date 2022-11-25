f71509b45f35e2920663b1260877e416f6a6a0bf
Galaxomas <thefidanabdulla@gmail.com>
2022-11-25 19:57:21 +0400

    Script saved

--- UnityTutorialGameProject/Assets/Scripts/MainMenuController.cs
+++ UnityTutorialGameProject/Assets/Scripts/MainMenuController.cs
@@ -10,9 +10,6 @@ public class MainMenuController : MonoBehaviour
         int selectedCharacter =
             int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
 
-        string clickedObj = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
-
-        Debug.Log(clickedObj);
 
         //SceneManager.LoadScene("Gameplay");
     }
